3cb7e9fd4f1ca0ad86b9291d588efa6d00b89115
TheLebaneseLegend <camconnelly20@outlook.com>
2025-12-04 19:05:34 -0500

    Day 1 Complete

--- Day 1/day_1.py
+++ Day 1/day_1.py
@@ -1,33 +1,27 @@
 import pandas as pd
-import math
 
 moves = pd.read_csv('input.txt', names=['moves'])
 moves = moves['moves'].tolist()
-# print(moves)
 
-current_number = 50
-ending_nums = []
+total = 50
+count = 0
+for i in moves:
+    if i[0] == "R":
+        i_new = int(i.replace("R", ""))
+        total = total + i_new
+        count = count + int(total / 100)
+        total = total % 100
+    else:
+        i_new = int(i.replace("L", ""))
+        old = total
+        total = total - i_new
+        if total == 0:
+            count = count + 1
+        if total < 0:
+            if old != 0:
+                count = count + 1
+            count = count + int((i_new - old) / 100)
+            total = total % 100
 
-for move in moves:
-    if move[0] == 'R':
-        current_number += (int(move[1:]) % 100)
-        if current_number > 99:
-            current_number -= 100
-            if current_number != 0 and int(math.floor((int(move[1:]) / 100))) != 0:
-                ending_nums += [0] * math.floor((int(move[1:]) / 100))
-
-
-    if move[0] == 'L':
-        current_number -= (int(move[1:]) % 100)
-        if current_number < 0:
-            current_number += 100
-            if current_number != 0 and int(math.floor((int(move[1:]) / 100))) != 0:
-                ending_nums += [0] * math.floor((int(move[1:]) / 100))
-
-    ending_nums.append(current_number)
-    print(move, current_number, math.floor((int(move[1:]) / 100)))
-
-code = ending_nums.count(0)
-
-print(code)
-
+print("total_end=", total)
+print("count_end=", count)
